1168be007170faa76174517a03e1448a20129230
Adithi19 <114479214+Adithi19@users.noreply.github.com>
2023-06-22 20:51:14 +0530

    Add files via upload

---
+++ Sociopedia/server/src/scenes/homePage/index.jsx
@@ -0,0 +1,66 @@
+import { Box, useMediaQuery } from "@mui/material";
+import { useSelector } from "react-redux";
+import Navbar from "scenes/navbar";
+import UserWidget from "scenes/widgets/UserWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget";
+import PostsWidget from "scenes/widgets/PostsWidget";
+import AdvertWidget from "scenes/widgets/AdvertWidget";
+import FriendListWidget from "scenes/widgets/FriendListWidget";
+import PlayersWidget from "scenes/widgets/PlayersWidget";
+import { Link } from 'react-router-dom';
+import { useTheme, Button } from "@mui/material";
+
+const HomePage = () => {
+  const { palette } = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
+
+  return (
+    <Box>
+      <Navbar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+        gap="0.5rem"
+        justifyContent="space-between"
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath} />
+          <PostsWidget userId={_id} />
+        </Box>
+        {isNonMobileScreens && (
+          <Box flexBasis="26%">
+            <AdvertWidget />
+            <PlayersWidget />
+            <Link to ="/players" target="_blank">
+              <Button
+                fullWidth
+                type="Show in New Page"
+                sx={{
+                m: "0.50rem 0",
+                p: "0.50rem",
+                backgroundColor: palette.primary.main,
+                color: palette.background.alt,
+                "&:hover": { color: palette.primary.main },
+                }}
+                >
+                    Show in New Page
+              </Button>
+            </Link>
+            <Box m="2rem 0" />
+            <FriendListWidget userId={_id} />
+          </Box>
+        )}
+      </Box>
+    </Box>
+  );
+};
+
+export default HomePage;
